2376b78cb4fd5db329475c96837fa470986412f5
Branko Anokic <brankoanokic@gmail.com>
2019-12-13 17:46:54 +0100

    Slider is pulling the data from JSON file

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Slider
 
 Trying out new ways of making slider.
-Using **HTML**, **CSS** and vanilla **JavaScript** in this project.
+Using **HTML**, **CSS** and vanilla **JavaScript AJAX** in this project.
 
-Slider is finished and dynamic so you can put as many items as you please.
+Data is pulled from a **JSON** file done with vanilla **JavaScript**
+
+Slider is finished. It's dynamic so you can put as many items as you please.
 
 ![Website image](https://i.ibb.co/wpB0kTR/screencapture-127-0-0-1-3000-2019-12-13-14-49-33.png)
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,60 +1,87 @@
 window.onload = () => {
 
-  const slider = document.querySelector('.slider-content');
-  const slide = document.querySelectorAll('.slide');
-  const rightArrow = document.querySelector('.arrow-right-container');
-  const leftArrow = document.querySelector('.arrow-left-container');
-  const first = slide[0];
-  const last = slide[slide.length - 1];
-  const jump_length = first.offsetWidth * -1;
-  var counter = 0;
+  /// AJAX CALL ///
 
-  console.log(jump_length + 20);
+  fetch('assets/js/item.json', { method : 'GET' }).then((responsive)=>{
+    return responsive.json();
+  }).then((data)=>{
+    sliderData(data);
+  })
 
-  slider.appendChild(first.cloneNode(true));
-  slider.prepend(last.cloneNode(true));
+  function sliderData(data){
+    let html = ``;
 
-  slider.appendChild(slide[1].cloneNode(true));
-  slider.prepend(slide[slide.length - 2].cloneNode(true));
-
-  slider.appendChild(slide[2].cloneNode(true));
-  slider.prepend(slide[slide.length - 3].cloneNode(true));
-
-  rightArrow.addEventListener('click', slideNext);
-  leftArrow.addEventListener('click', slideBack);
-
-  function slideNext() {
-
-      if(counter >= slide.length - 3) return;
-      counter++;
-      slider.style.transition = 'all .3s ease-in-out';
-      slider.style.left = ((jump_length + 20) * counter) +"px";
-
-      console.log(counter);
-
-  }
-
-
-  function slideBack() {
-      if(counter <= -3) return;
-      counter--;
-      slider.style.transition = 'all .3s ease-in-out';
-      slider.style.left = ((jump_length + 20) * counter) +"px";
-      console.log(counter);
-
-  }
-
-  slider.addEventListener('transitionend', checkIndex);
-
-  function checkIndex(){
-    if(counter >= slide.length - 3){
-      counter = -3;
-      slider.style.transition = 'none';
-      slider.style.left = ((jump_length + 20) * counter) +"px";
-    } else if(counter <= -3) {
-      counter = 3;
-      slider.style.transition = 'none';
-      slider.style.left = ((jump_length + 20) * counter) +"px";
+    for(item of data){
+      html += `<div class="slide">
+                  <img class="user-image" src="${item.img}" alt="Person image">
+                  <h4>${item.name}</h4>
+                  <span>${item.email}</span>
+                  <span><img src="assets/img/loc-icon.png" alt="icon">@${item.loc}</span>
+                </div>`
     }
+
+    document.querySelector('.slider-content').innerHTML = html;
+
+    slide();
   }
+
+  /// AJAX END ///
+
+  /// SLIDER ///
+
+  function slide() {
+    const slider = document.querySelector('.slider-content');
+    const slide = document.querySelectorAll('.slide');
+    const rightArrow = document.querySelector('.arrow-right-container');
+    const leftArrow = document.querySelector('.arrow-left-container');
+    const first = slide[0];
+    const last = slide[slide.length - 1];
+    const jump_length = first.offsetWidth * -1;
+    var counter = 0;
+
+    slider.appendChild(first.cloneNode(true));
+    slider.prepend(last.cloneNode(true));
+
+    slider.appendChild(slide[1].cloneNode(true));
+    slider.prepend(slide[slide.length - 2].cloneNode(true));
+
+    slider.appendChild(slide[2].cloneNode(true));
+    slider.prepend(slide[slide.length - 3].cloneNode(true));
+
+    rightArrow.addEventListener('click', slideNext);
+    leftArrow.addEventListener('click', slideBack);
+
+
+      function slideNext() {
+          if(counter >= slide.length - 3) return;
+          counter++;
+          slider.style.transition = 'all .3s ease-in-out';
+          slider.style.left = ((jump_length + 20) * counter) +"px";
+      }
+
+
+      function slideBack() {
+          if(counter <= -3) return;
+          counter--;
+          slider.style.transition = 'all .3s ease-in-out';
+          slider.style.left = ((jump_length + 20) * counter) +"px";
+      }
+
+      slider.addEventListener('transitionend', checkIndex);
+
+      function checkIndex(){
+        if(counter >= slide.length - 3){
+          counter = -3;
+          slider.style.transition = 'none';
+          slider.style.left = ((jump_length + 20) * counter) +"px";
+        } else if(counter <= -3) {
+          counter = 3;
+          slider.style.transition = 'none';
+          slider.style.left = ((jump_length + 20) * counter) +"px";
+        }
+      }
+  }
+
+  /// SLIDER END
+
 }
