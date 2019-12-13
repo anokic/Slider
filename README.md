8b5043d1dbef7b6db1a5ba47e112ecbe89f9c0b1
Branko Anokic <brankoanokic@gmail.com>
2019-12-13 14:51:09 +0100

    Slider is dynamic and finished

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Slider
 
-Trying out new ways of making slider. Using HTML, CSS and JavaScript in this poject.
+Trying out new ways of making slider.
+Using **HTML**, **CSS** and vanilla **JavaScript** in this poject.
+
+Slider is finished and dynamic so you can put as many items as you want.
+
+![Website image](https://i.ibb.co/wpB0kTR/screencapture-127-0-0-1-3000-2019-12-13-14-49-33.png)
--- assets/js/main.js
+++ assets/js/main.js
@@ -6,9 +6,10 @@ window.onload = () => {
   const leftArrow = document.querySelector('.arrow-left-container');
   const first = slide[0];
   const last = slide[slide.length - 1];
+  const jump_length = first.offsetWidth * -1;
   var counter = 0;
 
-  console.log(rightArrow);
+  console.log(jump_length + 20);
 
   slider.appendChild(first.cloneNode(true));
   slider.prepend(last.cloneNode(true));
@@ -18,41 +19,42 @@ window.onload = () => {
 
   slider.appendChild(slide[2].cloneNode(true));
   slider.prepend(slide[slide.length - 3].cloneNode(true));
-  // slider.style.left = "-" + (first.offsetWidth - 20) +"px";
 
   rightArrow.addEventListener('click', slideNext);
   leftArrow.addEventListener('click', slideBack);
 
   function slideNext() {
-      if(counter >= slide[slide.length + 2]) return;
-      counter--;
-      slider.style.transition = 'all .3s ease-in-out';
-      slider.style.left = ((first.offsetWidth - 20) * counter) +"px";
 
-    console.log(slide.length);
+      if(counter >= slide.length - 3) return;
+      counter++;
+      slider.style.transition = 'all .3s ease-in-out';
+      slider.style.left = ((jump_length + 20) * counter) +"px";
+
+      console.log(counter);
 
   }
 
 
   function slideBack() {
       if(counter <= -3) return;
-      counter++;
+      counter--;
       slider.style.transition = 'all .3s ease-in-out';
-      slider.style.left = ((first.offsetWidth - 20) * counter) +"px";
+      slider.style.left = ((jump_length + 20) * counter) +"px";
+      console.log(counter);
 
   }
 
   slider.addEventListener('transitionend', checkIndex);
 
   function checkIndex(){
-    if(counter == 3){
-      counter = slide.length - 6;
+    if(counter >= slide.length - 3){
+      counter = -3;
       slider.style.transition = 'none';
-      slider.style.left = ((first.offsetWidth - 20) * counter) +"px";
-    } else if(counter == -3) {
+      slider.style.left = ((jump_length + 20) * counter) +"px";
+    } else if(counter <= -3) {
       counter = 3;
       slider.style.transition = 'none';
-      slider.style.left = ((first.offsetWidth - 20) * counter) +"px";
+      slider.style.left = ((jump_length + 20) * counter) +"px";
     }
   }
 }
